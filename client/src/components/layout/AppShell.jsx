0f4f9a7a3ad375a6e06785df68d3d59974743ac7
ItzVion <vion4712@gmail.com>
2026-09-12 03:05:54 +0530

    feat(motion): mount global animation layer

--- client/src/components/layout/AppShell.jsx
+++ client/src/components/layout/AppShell.jsx
@@ -1,6 +1,7 @@
 import { Sidebar } from './Sidebar'
 import { Topbar } from './Topbar'
+import { AnimationLayer } from '../motion/AnimationLayer'
 
 export function AppShell({ children }) {
-  return <div className="min-h-screen bg-paper"><Sidebar/><div className="min-h-screen md:pl-64"><Topbar/><main className="px-5 pb-12 md:px-8">{children}</main></div></div>
+  return <div className="min-h-screen bg-paper"><AnimationLayer/><Sidebar/><div className="min-h-screen md:pl-64"><Topbar/><main className="px-5 pb-12 md:px-8">{children}</main></div></div>
 }
