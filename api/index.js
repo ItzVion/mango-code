617c0265ed14cbf828ecb400a8ec4cdab160283d
ItzVion <vion4712@gmail.com>
2026-09-14 22:33:24 +0530

    security: wire emergency controls into Vercel API

--- api/index.js
+++ api/index.js
@@ -1,1 +1,12 @@
-﻿export { default } from '../server/index.js'
+import app from '../server/index.js'
+import { emergencyMiddleware, securityStatus, activateLockdown, recoverLockdown } from '../server/security/emergencyLockdown.js'
+
+export default async function handler(req, res) {
+  if (req.url?.startsWith('/api/security/status')) return securityStatus(req, res)
+  if (req.url?.startsWith('/api/security/lockdown')) return activateLockdown(req, res)
+  if (req.url?.startsWith('/api/security/recover')) return recoverLockdown(req, res)
+
+  await new Promise((resolve) => emergencyMiddleware(req, res, () => resolve()))
+  if (res.headersSent) return
+  return app(req, res)
+}
